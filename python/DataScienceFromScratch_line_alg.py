68eedf37d64b90c80b3a5e5e39779ffe2b0b1885
perelygin <sfforum@yandex.ru>
2019-08-26 22:03:43 +0300

    26082019

--- python/DataScienceFromScratch_line_alg.py
+++ python/DataScienceFromScratch_line_alg.py
@@ -34,7 +34,23 @@ def  shape(A):#форма матрицы
     num_row = len(A)
     num_cols = len(A[0]) if A else 0  #число элементов в первой строке
     return num_row,num_cols
-    
+   
+def get_row(A,i):
+    return A[i]
+
+def get_column(A,j):
+    return [A_i[j] for A_i in A]
+
+
+def make_matrix(rows,cols,entry_fn):
+    return [[entry_fn(i,j)
+             for j in range(cols)]
+             for i in range(rows)]
+   
+def is_diagonal(i,j):
+    return 1 if i==j else 0
+
+
 v = [1,3,9]
 w = [2,1,4]
 e = [3,2,5]
@@ -54,3 +70,8 @@ B = [[1,2],
      [5,6]]
 
 print(shape(B))
+print(get_row(A,0))
+print(get_column(A,2))
+print(make_matrix(5,5,is_diagonal))
+
+
