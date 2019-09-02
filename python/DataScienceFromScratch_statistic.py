81ebf9f5ad2c0e0062fbacd2e5c65e33bb41f6d9
perelygin <sfforum@yandex.ru>
2019-09-02 23:02:05 +0300

    02092019

--- python/DataScienceFromScratch_statistic.py
+++ python/DataScienceFromScratch_statistic.py
@@ -29,4 +29,68 @@ largest_value = max(num_friends)
 print('максимальное значение "max(num_friends)"', largest_value)
 
 smallest_value = min(num_friends)
-print('максимальное значение "max(num_friends)"', largest_value)
+print('минимальное значение "min(num_friends)"', smallest_value)
+
+sorted_value = sorted(num_friends)
+#print('Отсортированные значения',sorted_value)
+
+smallest_value = sorted_value[0]
+print('минимальное значение ', smallest_value)
+second_smallest_value = sorted_value[1]
+print('следующее минимальное значение ', second_smallest_value)
+
+second_largest_value = sorted_value[-1]
+print('следующий максимум ', second_largest_value)
+
+def mean(x):
+    """Среднее"""
+    return sum(x)/len(x)
+
+print('среднее ',mean(num_friends))
+
+def median(v):
+    """ возвращает ближайшее к середине значение для v"""
+    n = len(v)
+    sorted_v = sorted(v)
+    midpoint = n // 2  #индекс серединного значения
+    if n % 2 == 1: # если нечетное, то вернуть срединное значение
+        return sorted_v[midpoint]
+    else: # если четное, то вернуть среднее значение 2х значений из середины
+        lo = midpoint-1
+        hi = midpoint
+        return (sorted_v[lo]+sorted_v[hi]) / 2
+
+print('медиана ',median(num_friends))
+#print((sorted(num_friends)[124]+sorted(num_friends)[125])/2)
+
+def quantile(x,p):
+    """возвращает значение в x, соответствующее p-му проценту данных """
+    p_index = int(p*len(x))   
+    return sorted(x)[p_index]
+
+print('Верхний квантиль ',quantile(num_friends,0.75))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
